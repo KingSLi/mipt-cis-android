a826e84c546d0e9d9fa234272e760d0bbd8b15e5
KingSLi <heroesandang@mail.ru>
2021-05-20 04:06:17 +0300

    add

---
+++ hw3/app/src/main/java/edu/phystech/cis/android/Profile.kt
@@ -0,0 +1,12 @@
+package edu.phystech.cis.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
